7515d921952971c5ec985640519da92e17ece878
Jérémy Cheynet <jerem.cheynet@gmail.com>
2014-05-26 14:58:06 +0200

    Main prog which compute fft for signal :)
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

--- main.c
+++ main.c
@@ -7,19 +7,30 @@
 #include "adc.h"
 #include "io.h"
 #include "tim0.h"
+#include "uart.h"
 
 int main()
 {
-	adc_init(ADC_PRESCALER_2, ADC0);
+	adc_init(ADC_PRESCALER_4, ADC0);
 	io_init();
 	tim0_init();
 	fft_init();
+	uart_init();
 	sei();
-	_delay_ms(10);
 
 	while(TRUE)
 	{
-		fft_compute_fft();
+		uint8_t i;
+		cli();
+		for (i = 0; i < SAMPLE_SIZE; ++i)
+		{
+			uart_send_uint8_t(i);
+			uart_send_char(';');
+			uart_send_uint32_t((uint32_t)fft_compute_fft(i));
+			uart_send_ln();
+		}
+		sei();
+		_delay_ms(10000);
 	}
 	return EXIT_SUCCESS;
 }

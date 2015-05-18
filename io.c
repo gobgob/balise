65e8c6a1e78c77e5566996d184d06f255da65bf0
Jérémy Cheynet <jerem.cheynet@gmail.com>
2015-05-18 10:49:57 +0200

    Add other LED debug (like on schematic)
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

--- io.c
+++ io.c
@@ -7,6 +7,8 @@
 void io_init()
 {
 	sbi(CONCAT(DDR, PORT_BLINK), CONCAT_3(PORT, PORT_BLINK, PIN_BLINK));
+	sbi(CONCAT(DDR, PORT_GREEN), CONCAT_3(PORT, PORT_GREEN, PIN_GREEN));
+	sbi(CONCAT(DDR, PORT_BLUE), CONCAT_3(PORT, PORT_BLUE, PIN_BLUE));
 }
 
 void io_blink_toggle()
@@ -22,6 +24,36 @@ void io_blink_on()
 void io_blink_off()
 {
 	cbi(CONCAT(PORT, PORT_BLINK), CONCAT_3(PORT, PORT_BLINK, PIN_BLINK));
+}
+
+void io_green_toggle()
+{
+	tbi(CONCAT(PORT, PORT_GREEN), CONCAT_3(PORT, PORT_GREEN, PIN_GREEN));
+}
+
+void io_green_on()
+{
+	sbi(CONCAT(PORT, PORT_GREEN), CONCAT_3(PORT, PORT_GREEN, PIN_GREEN));
+}
+
+void io_green_off()
+{
+	cbi(CONCAT(PORT, PORT_GREEN), CONCAT_3(PORT, PORT_GREEN, PIN_GREEN));
+}
+
+void io_blue_toggle()
+{
+	tbi(CONCAT(PORT, PORT_BLUE), CONCAT_3(PORT, PORT_BLUE, PIN_BLUE));
+}
+
+void io_blue_on()
+{
+	sbi(CONCAT(PORT, PORT_BLUE), CONCAT_3(PORT, PORT_BLUE, PIN_BLUE));
+}
+
+void io_blue_off()
+{
+	cbi(CONCAT(PORT, PORT_BLUE), CONCAT_3(PORT, PORT_BLUE, PIN_BLUE));
 }
 
 ISR(INT0_vect)
--- io.h
+++ io.h
@@ -4,9 +4,21 @@
 #define PORT_BLINK B
 #define PIN_BLINK 5
 
+#define PORT_GREEN D
+#define PIN_GREEN 4
+
+#define PORT_BLUE D
+#define PIN_BLUE 5
+
 void io_init();
 void io_blink_toggle();
 void io_blink_on();
 void io_blink_off();
+void io_green_toggle();
+void io_green_on();
+void io_green_off();
+void io_blue_toggle();
+void io_blue_on();
+void io_blue_off();
 
 #endif
